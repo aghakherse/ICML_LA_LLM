7d827f1f2d7f97cc481a6d6f9063abd3e9761f46
aghakherse <doblood@yahoo.com>
2025-04-04 18:06:31 -0400

    Update profiling.py

--- linear_attention/profiling.py
+++ linear_attention/profiling.py
@@ -8,7 +8,7 @@ from torch.autograd import gradcheck
 from Linear_Attention import FASTMultiHeadAttention
 import fastmax_cuda
 import numpy as np
-
+from gla import GatedLinearAttention
 
 LA_ours = FASTMultiHeadAttention() # ours linear attention implementation
 LA_torch = FASTMultiHeadAttention(False) # linear attention implemented using pytorch
@@ -18,11 +18,11 @@ LA_torch = FASTMultiHeadAttention(False) # linear attention implemented using py
 # look here
 b = 4 # batch
 h = 16 # head
-d = 64 # dimension per head (i.e. embedding_dimension/h)
+d = 128 # dimension per head (i.e. embedding_dimension/h)
 
 # n changes from 10^strt to 10^endd. The number of test points are count
 rep = 100
-count = 4
+count = 10
 strt = 3 # log scale
 endd = 5 # log scale
 # lengths = [1024,4096,8192]
@@ -34,92 +34,117 @@ print("bhd = ",b,",",h,",",d,",")
 reg_attention_time = np.zeros(count)
 our_LA_time = np.zeros(count)
 torch_LA_time = np.zeros(count)
+gla_time = np.zeros(count)
 reg_attention_memory = np.zeros(count)
 our_LA_memory = np.zeros(count)
 torch_LA_memory = np.zeros(count)
+gla_memory = np.zeros(count)
 device = torch.device(0)
-mask = True
+mask = False
 
 
-j = -1
-print("Our LA Implementation")
-for i in np.logspace(strt, endd, count):
-# for i in lengths:
-    try:
-        j += 1
-        print(int(i))
-        for ii in range(rep):
-            # print(ii)
-            torch.cuda.empty_cache()
-            q = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'),requires_grad=True, dtype=dtype).contiguous()
-            k = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'),requires_grad=True, dtype=dtype).contiguous()
-            v = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'),requires_grad=True, dtype=dtype).contiguous()
-            start_time = time.time()
-            e = LA_ours(q,k,v,mask)
-            print(e)
-            cuda.synchronize()
-            end_time = time.time()
-            our_LA_time[j] += (end_time - start_time)/rep
-        our_LA_memory[j] = torch.cuda.memory_allocated()
-        # print(torch.cuda.memory_allocated())
-    except:
-        print("OOM for token length of ", int(i))
+# j = -1
+# print("Our LA Implementation")
+# for i in np.logspace(strt, endd, count):
+# # for i in lengths:
+#     try:
+#         j += 1
+#         print(int(i))
+#         for ii in range(rep):
+#             # print(ii)
+#             torch.cuda.empty_cache()
+#             q = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'),requires_grad=True, dtype=dtype).contiguous()
+#             k = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'),requires_grad=True, dtype=dtype).contiguous()
+#             v = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'),requires_grad=True, dtype=dtype).contiguous()
+#             start_time = time.time()
+#             e = LA_ours(q,k,v,mask)
+#             # print(e)
+#             cuda.synchronize()
+#             end_time = time.time()
+#             our_LA_time[j] += (end_time - start_time)/rep
+#         our_LA_memory[j] = torch.cuda.memory_allocated()
+#         # print(torch.cuda.memory_allocated())
+#     except:
+#         print("OOM for token length of ", int(i))
 
+
+# print("############################################")
+# print("Regular Attention")
+
+# j = -1
+# for i in np.logspace(strt, endd, count):
+# #   for i in lengths:
+#     try:
+#         j += 1
+#         print(int(i))
+#         if(i > 500000): continue
+#         for ii in range(rep):
+#             torch.cuda.empty_cache()
+#             q = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
+#             k = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
+#             v = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
+
+#             start_time = time.time()
+#             e = F.scaled_dot_product_attention(q, k, v,is_causal=True)
+#             # e = torch.sum(F.scaled_dot_product_attention(q, k, v,is_causal=True)).backward(retain_graph=True)
+#             cuda.synchronize()
+#             end_time = time.time()
+#             reg_attention_time[j] += (end_time - start_time)/rep
+#         reg_attention_memory[j] = torch.cuda.memory_allocated()
+#         # print(torch.cuda.memory_allocated())
+#     except:
+#         print("OOM for token length of ", int(i))
+
+
+# print("############################################")
+
+# j = -1
+# print("Pytorch LA Implementation")
+# for i in np.logspace(strt, endd, count):
+# # for i in lengths:
+#     try:
+#         j += 1
+#         print(int(i))
+#         for ii in range(rep):
+#             torch.cuda.empty_cache()
+#             q = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
+#             k = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
+#             v = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
+#             start_time = time.time()
+#             e = LA_torch(q,k,v,mask = mask)
+#             # e = torch.sum(fastmax_torch(q,k,v,mask = mask)).backward(retain_graph=True)
+#             # print(e)
+#             cuda.synchronize()
+#             end_time = time.time()
+            
+#             torch_LA_time[j] += (end_time - start_time)/rep
+#         torch_LA_memory[j] = torch.cuda.memory_allocated()
+#         # print(torch.cuda.memory_allocated())
+#     except:
+#         print("OOM for token length of ", int(i))
 
 print("############################################")
-print("Regular Attention")
-
+print("Gated LA Implementation")
+gla = GatedLinearAttention(d = d, h = h, device = torch.device('cuda'))
 j = -1
-for i in np.logspace(strt, endd, count):
-#   for i in lengths:
-    try:
-        j += 1
-        print(int(i))
-        if(i > 500000): continue
-        for ii in range(rep):
-            torch.cuda.empty_cache()
-            q = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
-            k = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
-            v = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
-
-            start_time = time.time()
-            e = F.scaled_dot_product_attention(q, k, v,is_causal=True)
-            # e = torch.sum(F.scaled_dot_product_attention(q, k, v,is_causal=True)).backward(retain_graph=True)
-            cuda.synchronize()
-            end_time = time.time()
-            reg_attention_time[j] += (end_time - start_time)/rep
-        reg_attention_memory[j] = torch.cuda.memory_allocated()
-        # print(torch.cuda.memory_allocated())
-    except:
-        print("OOM for token length of ", int(i))
-
-
-print("############################################")
-
-j = -1
-print("Pytorch LA Implementation")
 for i in np.logspace(strt, endd, count):
 # for i in lengths:
-    try:
+    # try:
         j += 1
         print(int(i))
         for ii in range(rep):
             torch.cuda.empty_cache()
-            q = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
-            k = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
-            v = torch.normal(0,1,[b,h,int(i),d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
+            x = torch.normal(0,1,[b,int(i),h*d],device=torch.device('cuda'), dtype=dtype,requires_grad=True)
             start_time = time.time()
-            e = LA_torch(q,k,v,mask = mask,p = p)
-            # e = torch.sum(fastmax_torch(q,k,v,mask = mask,p = p)).backward(retain_graph=True)
-            print(e)
+            e = gla(x)
             cuda.synchronize()
             end_time = time.time()
             
-            torch_LA_time[j] += (end_time - start_time)/rep
-        torch_LA_memory[j] = torch.cuda.memory_allocated()
+            gla_time[j] += (end_time - start_time)/rep
+        gla_memory[j] = torch.cuda.memory_allocated()
         # print(torch.cuda.memory_allocated())
-    except:
-        print("OOM for token length of ", int(i))
+    # except:
+    #     print("OOM for token length of ", int(i))
 
 # print("\n softmax = \n")
 # print("time")
@@ -129,30 +154,41 @@ temp += "]"
 print("Reg. Att. Time = ", temp)
 # print("memory")
 temp = "["
-for i in soft_memory: temp += str(i) + ", "
+for i in reg_attention_memory: temp += str(i) + ", "
 temp += "]"
 print("Reg. Att. Memory = ", temp)
 print()
 # print("LA with our implementation = \n")
 # print("time")
 temp = "["
-for i in fast_time_custom: temp += str(i) + ", "
+for i in our_LA_time: temp += str(i) + ", "
 temp += "]"
 print("Our LA Time = ", temp)
 # print("memory")
 temp = "["
-for i in fast_memory_custom: temp += str(i) + ", "
+for i in our_LA_memory: temp += str(i) + ", "
 temp += "]"
 print("Our LA Memory = ", temp)
 print()
 # print("\n LA with Pytorch implementation = \n")
 # print("time")
 temp = "["
-for i in fast_time: temp += str(i) + ", "
+for i in torch_LA_time: temp += str(i) + ", "
 temp += "]"
 print("Pythorch LA Time = ", temp)
 # print("memory")
 temp = "["
-for i in fast_memory: temp += str(i) + ", "
+for i in torch_LA_memory: temp += str(i) + ", "
 temp += "]"
 print("Pytorch LA Memory = ", temp)
+
+print()
+temp = "["
+for i in gla_time: temp += str(i) + ", "
+temp += "]"
+print("Gated LA Time = ", temp)
+# print("memory")
+temp = "["
+for i in gla_memory: temp += str(i) + ", "
+temp += "]"
+print("Gated LA Memory = ", temp)
